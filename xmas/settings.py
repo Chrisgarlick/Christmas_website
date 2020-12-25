a0170c604e320a1c019ddf48e5966b9feb9f2ca6
Chris Garlick <67547010+Chrisgarlick@users.noreply.github.com>
2020-12-25 14:20:41 +0000

    Update settings.py

--- xmas/settings.py
+++ xmas/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z&j52f01a%-8y*65xw4st1m+_2v2-p6vu@jc3+!n90ig)5f5o4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
